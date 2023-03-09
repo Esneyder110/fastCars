83436153ea29addb798aecbfef04557440d7cce9
Esneyder Monsalve <esneyder110@gmail.com>
2023-03-09 17:59:57 -0400

    implement env variables to make the connection to the DB

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigType } from '@nestjs/config';
+import config from 'src/configuration/config';
+
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://root:root@localhost:27017/?authMechanism=DEFAULT',
-    ),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigType<typeof config>) => {
+        const { connection, user, password, host, port, dbName } =
+          configService.mongo;
+
+        return {
+          uri: `${connection}://${host}:${port}`,
+          user,
+          pass: password,
+          dbName,
+        };
+      },
+      inject: [config.KEY],
+    }),
   ],
   exports: [MongooseModule],
 })
