df633024734d302c2bf279a1449f29aaceec67cc
Esneyder Monsalve <esneyder110@gmail.com>
2023-03-16 18:41:47 -0400

    add register route for a user new user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 import { AuthService } from './auth.service';
 import { Public } from './decorators/public/public.decorator';
@@ -9,9 +10,17 @@ import { LocalAuthGuard } from './guards/local-auth/local-auth.guard';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  // TODO tipar el dato que me envia
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req: any) {
     return this.authService.login(req.user);
+  }
+
+  @Public()
+  @Post('register')
+  async register(@Body() payload: CreateUserDto) {
+    // Guardar el nuevo usuario en la base de datos
+    return this.authService.register(payload);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
 import { UsersService } from 'src/users/users.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -19,10 +20,17 @@ export class AuthService {
     }
     return null;
   }
+
+  // tipar el dato que me envia
   async login(user: any) {
     const payload = { username: user.username, sub: user.userId };
     return {
       access_token: this.jwtService.sign(payload),
     };
+  }
+
+  async register(payload: CreateUserDto) {
+    const user = await this.usersService.create(payload);
+    return this.login(user);
   }
 }
